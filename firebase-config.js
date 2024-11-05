ba73e520f17291df8b93e9c232e053fa072d344e
m1rox17 <yaroslavkostenko34@gmail.com>
2024-11-05 20:52:06 +0200

    Dependency installation, basic configuration files
    Installing, customising tailwind and firebase (setup for google auth). Changing page title, removing unnecessary files and code

---
+++ firebase-config.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import {} from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDVfvHXsO4aKivxX7MJRIP8BI-tI7X6UVg",
+  authDomain: "chat-961d1.firebaseapp.com",
+  projectId: "chat-961d1",
+  storageBucket: "chat-961d1.firebasestorage.app",
+  messagingSenderId: "1018232357067",
+  appId: "1:1018232357067:web:f88e875a4b890352901b4e",
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider();
